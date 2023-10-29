3b16f6f8b1ad07428eedf197722166f70f075b91
Charlie Latimer <clatimer159@gmail.com>
2023-10-29 10:36:08 +0000

    Added bootloader and backspace combo

--- keyboards/ferris/keymaps/combo_latimer/keymap.c
+++ keyboards/ferris/keymaps/combo_latimer/keymap.c
@@ -1,15 +1,15 @@
 #include <stdint.h>
 #include QMK_KEYBOARD_H
 #include "keycodes.h"
+// #include "g/keymap_combo.h"
 
 // Define combos
-enum combos {
-    JK_ESC
-};
 
 const uint16_t PROGMEM escape_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM backspace_combo[] = {KC_D, KC_F, COMBO_END};
 combo_t key_combos[] = {
     COMBO(escape_combo, KC_ESC),
+    COMBO(backspace_combo, KC_BACKSPACE),
 };
 
 
@@ -19,19 +19,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,         KC_W,         KC_E,        KC_R, KC_T,                     KC_Y, KC_U, KC_I, KC_O, KC_P,
                 KC_A,         KC_S,         KC_D,        KC_F, KC_G,                     KC_H, KC_J, KC_K, KC_L, KC_SCLN,
                 KC_Z,         KC_X,         KC_C,        KC_V, KC_B,                     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                                                         MO(_MODS), KC_LSFT,                 KC_SPC, MO(_NUMBERS)
+                                                         OSL(_MODS), KC_LSFT,                 KC_SPC, MO(_NUMBERS)
     ),
     [_NUMBERS]    = LAYOUT_split_3x5_2(
-        XXXXXXX, UK_AT, XXXXXXX, XXXXXXX, XXXXXXX,                KC_ASTR, KC_7, KC_8, KC_9, KC_PLUS,
+        MO(_FUNC), UK_AT, XXXXXXX, XXXXXXX, XXXXXXX,                KC_ASTR, KC_7, KC_8, KC_9, KC_PLUS,
         KC_LT, KC_SLASH, KC_LCBR, KC_LPRN, KC_LBRC,                KC_SLASH, KC_4, KC_5, KC_6, KC_MINUS,
         KC_GT, KC_NUBS, KC_RCBR, KC_RPRN, KC_RBRC,                KC_0, KC_1, KC_2, KC_3, KC_EQL,
-                                    MO(_SYM), KC_LSFT,                   XXXXXXX, XXXXXXX
+                                    OSL(_SYM), KC_LSFT,                   MO(_BOOTLOADER), XXXXXXX
     ),
     [_MODS]    = LAYOUT_split_3x5_2(
         KC_ESC, XXXXXXX, XXXXXXX, LCTL(KC_A), XXXXXXX,                KC_PGUP, KC_HOME, KC_UP, KC_END, KC_INS,
         OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL), XXXXXXX,                KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PSCR,
         LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_LCMD,                KC_DELETE, KC_BACKSPACE, KC_TAB, KC_APP, XXXXXXX,
-                                               XXXXXXX, XXXXXXX,        KC_ENTER, MO(_FUNC)
+                                               XXXXXXX, MO(_BOOTLOADER),        KC_ENTER, OSL(_FUNC)
     ),
     [_FUNC]    = LAYOUT_split_3x5_2(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_F12, KC_F7, KC_F8, KC_F9, XXXXXXX,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX
     ),
     [_BOOTLOADER]    = LAYOUT_split_3x5_2(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOTLOADER,                QK_BOOTLOADER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX
