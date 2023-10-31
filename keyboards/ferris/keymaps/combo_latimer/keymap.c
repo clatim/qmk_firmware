20d548bc87f3fef7f0b1a200a3cfb87da6d4a095
Charlie Latimer <clatimer159@gmail.com>
2023-10-31 19:13:13 +0000

    Changed the following:
    - added a combo for tab, however it doesn't work with the one shot
       modifiers (e.g. alt+tab)
    - added a combo for delete
    - removed the repeat key for now and put ; back.
    - remove the OSL from modifier layer as it was messing with me.
    - added a combo for tmux

--- keyboards/ferris/keymaps/combo_latimer/keymap.c
+++ keyboards/ferris/keymaps/combo_latimer/keymap.c
@@ -21,7 +21,7 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_SQUARE] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
     [TD_ANGLE] = ACTION_TAP_DANCE_DOUBLE(KC_LT, KC_GT),
     [TD_SLASH] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_NUBS),
-    [TD_QUOTE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_AT),
+    [TD_QUOTE] = ACTION_TAP_DANCE_DOUBLE(KC_AT, KC_QUOTE),
     [TD_BAR] = ACTION_TAP_DANCE_DOUBLE(KC_UNDS, LSFT(KC_NUBS)),
 };
 
@@ -30,9 +30,9 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_2(
                 KC_Q,         KC_W,         KC_E,        KC_R, KC_T,                     KC_Y, KC_U, KC_I, KC_O, KC_P,
-                KC_A,         KC_S,         KC_D,        KC_F, KC_G,                     KC_H, KC_J, KC_K, KC_L, QK_REPEAT_KEY,
+                KC_A,         KC_S,         KC_D,        KC_F, KC_G,                     KC_H, KC_J, KC_K, KC_L, KC_SCLN,
                 KC_Z,         KC_X,         KC_C,        KC_V, KC_B,                     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                                                         OSL(_MODS), KC_LSFT,                 KC_SPC, MO(_NUMBERS)
+                                                         MO(_MODS), KC_LSFT,                 KC_SPC, MO(_NUMBERS)
     ),
     [_NUMBERS]    = LAYOUT_split_3x5_2(
         KC_CIRC,      UK_AT,        KC_GRAVE,     TD(TD_QUOTE),   MO(_FUNC),                    KC_EQL,  KC_7, KC_8, KC_9, KC_PLUS,
